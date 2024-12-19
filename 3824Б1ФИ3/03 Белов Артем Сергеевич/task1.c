c9a01f81b2fb7edc2d221595b05000947c35c48e
sadako16 <CCC.Art@yandex.com>
2024-12-19 09:57:57 +0300

    Update task1.c

--- 3824Б1ФИ3/03 Белов Артем Сергеевич/task1.c
+++ 3824Б1ФИ3/03 Белов Артем Сергеевич/task1.c
@@ -2,5 +2,11 @@
 // Вычислить сумму S тех элементов массива, значения которых совпадают с их индексами.
 unsigned task1(unsigned A[], size_t n)
 {
-  return -1;
-}
+   for (size_t i = 0; i < n - 1; i++)
+   {
+     if (A[i] == [i])
+     {
+       sum += A[i];
+     }
+     return sum;
+   }
