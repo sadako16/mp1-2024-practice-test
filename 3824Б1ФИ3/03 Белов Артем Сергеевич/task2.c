e882039b4aa07a228a076f4e9117228c884ecd4a
sadako16 <CCC.Art@yandex.com>
2024-12-19 10:12:01 +0300

    Update task2.c

--- 3824Б1ФИ3/03 Белов Артем Сергеевич/task2.c
+++ 3824Б1ФИ3/03 Белов Артем Сергеевич/task2.c
@@ -1,6 +1,29 @@
 // Задача 2. Дан непустой массив A, содержащий n вещественных чисел.
 // Определить количество разных элементов массива.
 size_t task2(double A[], size_t n)
+void bubblesort(double arr[], int size) {
+ int i, temp, j, r;
+ r = 0;
+ for (j = 0; j != size; j++) {
+  for (i = 0; i < size - 1; i++) {
+   if (arr[i] > arr[i + 1])
+   {
+    temp = arr[i];
+    arr[i] = arr[i + 1];
+    arr[i + 1] = temp;
+    r++;
+   }
+  }
+ }
+}
+
 {
-  return 0;
+  for (int i = 0; i < n-1; i++)
+    {
+      if (A[i] != A[i+1])
+      {
+        k += 1
+      }
+    }
+  return k
 }
